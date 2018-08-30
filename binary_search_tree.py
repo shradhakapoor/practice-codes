870af0ce9bbae5dbf6a3c28c6105e358bcb80722
Shradha <shradha_kapoor@outlook.com>
2018-08-29 23:13:28 -0700

    binary tree

---
+++ binary_search_tree.py
@@ -0,0 +1,62 @@
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class Binary_Search_Tree(object):
+    def __init__(self, value):
+        self.root = Node(value)
+
+    # insert element
+
+    # delete an element
+
+    # find an element, with recursion
+
+    # find an element, without recursion
+
+    # check if binary tree is BST or not, complexity O(n^2)
+
+    # check if binary tree is BST or not, complexity O(n)
+
+    # find minimum element, with recursion
+
+    # find minimum element, without recursion
+
+    # find maximum element, with recursion
+
+    # find maximum element, without recursion
+
+    # find k-th smallest element
+
+    # tell the successor of a node in BST
+
+    # tell the predecessor of a node in BST
+
+    # given 2 nodes, find the lowest common ancestor in BST
+
+    # find shortest path between 2 nodes
+
+    # count number of BSTs possible with n nodes
+
+    # convert BST to circular doubly linked list with space complexity(1)
+
+    # convert sorted doubly linked list to balanced BST
+
+    # convert sorted array to BST, time complexity O(n)
+
+    # convert singly linked list (sorted in ascending order) to height balanced BST
+
+    # find floor of given data (largest data <= given data)
+
+    # find ceiling of given data (smallest data > given data)
+
+    # print all elements in range r1 to r2
+
+    # trim the given BST, and form a new tree with all the elements between r1 to r2
+
+    # check whether elements of 2 BSTs are same or not (order of elements doesn't matter)
+
+
+
