7974ff1a6e3afd2a7a4130553f36d309f4fbdbbf
Shradha <shradha_kapoor@outlook.com>
2018-09-10 01:35:47 -0700

    Binary Search tree

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -74,9 +74,11 @@ class Binary_Search_Tree(object):
     def _print( self, node ):
         if node is None:
             return
+        if node.left:
+            self._print( node.left )
         print(node.value, end=' ')
-        self._print(node.left)
-        self._print(node.right)
+        if node.right:
+            self._print(node.right)
 
     # find an element, with recursion
     def search_with_recursion( self, item ):
@@ -234,31 +236,225 @@ class Binary_Search_Tree(object):
         else:
             return 1+ self.distance_from_root(node.right, value)
 
-    # count number of BSTs possible with n nodes
+    # count number of BSTs possible with n nodes = catalan number (2nCn)/n+1 = 2n! / (n+1! n!)
+    # count of number of Binary Trees with n nodes  = n! * catalan number
+    def count_of_BST_possible( self, n ):
+        if n is None:
+            return 0
+
+        # calculate 2n!
+        twice_n = 1
+        tn = (2 * n) + 1
+        for i in range(1, tn):
+            twice_n = twice_n * i
+        # calculate n!
+        single_n = 1
+        for i in range(1, n+1):
+            single_n *= i
+        # find n+1! by n! * n+1
+        n_1 = single_n * (n + 1)
+        # result = catalan number
+        result = twice_n / (single_n * n_1)
+
+        return result
 
     # convert BST to circular doubly linked list with space complexity(1)
+    prev = None
+
+    def bst_to_circular_doubly_linkedlist( self, root, head ):
+        # initially, root points to root of the linked list and head is None
+
+        if root is None: return
+
+        # basically we are doing inorder traversal and inside we write steps for converting bst to dll
+
+        self.bst_to_circular_doubly_linkedlist(root.left, head)
+
+        if self.prev is None:
+            head = root
+        else:
+            root.left = self.prev
+            self.prev.right = root
+
+        # make current node as prev node
+        self.prev = root
+
+        self.bst_to_circular_doubly_linkedlist(root.right, head)
+
+    # convert sorted array to BST, time complexity O(n)
+    def sorted_array_to_bst( self, arr ):
+        if len(arr) == 0: return
+        if len(arr) == 1:
+            root = Node(arr[0])
+            return root
+        mid = int(len(arr)/2)
+        root = Node(arr[mid])
+
+        if arr[mid-1] < arr[mid+1]:
+            arr1 = arr[0:mid]
+            arr2 = arr[(mid+1):]
+        else:
+            arr1 = arr[(mid + 1):]
+            arr2 = arr[0:mid]
+
+        root.left = self.sorted_array_to_bst(arr1)
+        root.right = self.sorted_array_to_bst(arr2)
+
+        return root
+
+    # find floor of given data (largest data <= given data)
+    def floor_of_given_value( self, root, value ):
+        if root is None:
+            return -1
+        if root.value == value: return root.value
+
+        if root.value > value: return self.floor_of_given_value(root.left, value)
+
+        right_value = self.floor_of_given_value(root.right, value)
+        return right_value if right_value >= value else root.value
+
+    # find ceiling of given data (smallest data > given data)
+    def ceil_of_given_value( self, root, value ):
+        if root is None:
+            return -1
+
+        # equal to key value
+        if value == root.value: return root.value
+
+        # if root.value < value, ceil must be in right subtree
+        if root.value < value: return  self.ceil_of_given_value(root.right, value)
+
+        # if value < root.value, ceil is either root.value or in left subtree
+        left_value = self.ceil_of_given_value(root.left, value)
+        return left_value if left_value >= value else root.value
+
+    # print all elements in increasing order in range r1 to r2
+    def print_all_in_range( self, root, r1, r2 ):
+        if root is None:
+            return
+
+        # since desired output is sorted, recurse for left subtree first
+        # if root.value > r1 only then we can get elements in left subtree
+        if r1 < root.value:
+            self.print_all_in_range(root.left, r1, r2)
+
+        if r1 <= root.value <= r2:
+            print (root.value, end=', ')
+
+        # if r2 > root.value only then we can get elements in right subtree
+        if r2 > root.value:
+            self.print_all_in_range(root.right, r1, r2)
+
+    # remove BST elements outside the given range, and modified tree should also be BST
+    def remove_elements_outside_range( self, root, r1, r2 ):
+        if root is None: return
+
+        # basically we need to fix the tree in post-order fashion
+        root.left = self.remove_elements_outside_range(root.left, r1, r2)
+        root.right = self.remove_elements_outside_range(root.right, r1, r2)
+
+        # now fix the root if root is not in range
+        # case 1: root.value < r1, delete root and return right subtree
+        if root.value < r1:
+            right_child = root.right
+            self.delete(root, root.value)
+            return right_child
+        # case 2: if root.value > r2, delete root and return left subtree
+        if root.value > r2:
+            left_child = root.left
+            self.delete(root, root.value)
+            return left_child
+
+        return root
+
+    # delete an element
+    def delete( self, root, item):
+        if root is None:
+            return False
+
+        # if value to be deleted is less than root then the value lies in left subtree
+        if item < root.value:
+            root.left = self.delete(root.left, item)
+
+        # if value to be deleted is greater than root then the value lies in right subtree
+        elif item > root.value:
+            root.right = self.delete(root.right, item)
+
+        # else the value is same as root.value
+        else:
+            # case 1: node to be deleted has 1 child or no child
+            if root.left is None:
+                temp = root.right
+                root = None
+                return temp
+            if root.right is None:
+                temp = root.left
+                root = None
+                return temp
+
+            # case 2: node to be deleted has 2 children
+
+            # get the inorder successor of root node
+            temp = root.right
+            while temp.left is not None:
+                temp = temp.left
+
+            smallest_value = temp
+            # copy inorder successor node.value to this root.value
+            root.value = smallest_value.value
+
+            # delete the inorder successor
+            root.right = self.delete(root.right, smallest_value.value)
+
+        return root
+
+    # tell inorder successor of given key in BST
+    def inorder_successor( self, node ):
+        if node is None: return
+
+        # case 1: if node has right subtree then find least value node from that right subtree
+        if node.right:
+            temp = node.right
+            while temp.left: temp = temp.left
+            return temp
+
+        # case 2: if node doesn't have right subtree
+        # then search that node starting from root and the node from where we take the last left is the answer
+        root = self.root
+        store = None
+        while root.value != node.value:
+            if node.value < root.value:
+                store = root
+                root = root.left
+            else: root = root.right
+        return store
+
+    # tell the predecessor of a node in BST
+    def inorder_predecessor( self, node ):
+        if node is None: return
+
+        # case 1: if node has left subtree then find the max value node from that left subtree
+        if node.left:
+            temp = node.left
+            while temp.right: temp = temp.right
+            return temp
+
+        # case 2: if node doesn't have left subtree
+        # then search that node starting from root and the node from where we take the last right is the answer
+        root = self.root
+        store = None
+        while root.value != node.value:
+            if node.value > root.value:
+                store = root
+                root = root.right
+            else: root = root.left
+        return store
 
     # convert sorted doubly linked list to balanced BST
 
-    # convert sorted array to BST, time complexity O(n)
-
-    # convert singly linked list (sorted in ascending order) to height balanced BST
-
-    # find floor of given data (largest data <= given data)
-
-    # find ceiling of given data (smallest data > given data)
-
-    # print all elements in range r1 to r2
-
-    # trim the given BST, and form a new tree with all the elements between r1 to r2
-
     # check whether elements of 2 BSTs are same or not (order of elements doesn't matter)
 
-    # delete an element
-
-    # tell inorder successor of given key in BST
-
-    # tell the predecessor of a node in BST
+    # convert singly linked list (sorted in ascending order) to height balanced BST
 
 
 # BST
@@ -307,4 +503,29 @@ print('kth smallest element in tree:', str(bst.kth_smallest_element(2)))
 
 print('lowest common ancestor of two nodes:', str(bst.lowest_common_ancestor(bst.root, 700, 900).value))
 
-print('shortest distance between two nodes:', str(bst.shortest_distance_between_two_nodes(bst.root, 200, 450)) )
+print('shortest distance between two nodes:', str(bst.shortest_distance_between_two_nodes(bst.root, 200, 450)))
+
+print('count of number of BSTs possible with n nodes:', str(bst.count_of_BST_possible(7)))
+
+print('\nConvert sorted array to BST:', end=' ')
+bst._print(bst.sorted_array_to_bst([70,60,50,40,30,20,10]))
+
+print('\nFloor of given value:', bst.floor_of_given_value(bst.root, 410))
+print('ceiling of given value:', bst.ceil_of_given_value(bst.root, 720))
+
+print('Inorder successor of node:', str(bst.inorder_successor(bst.root).value))
+print('Inorder predecessor of node:', str(bst.inorder_predecessor(bst.root).value))
+
+print('Print all elements in the range r1 to r2 in increasing order:')
+bst.print_all_in_range(bst.root, 100, 750)
+
+print('\nRemove all elements outside the range:', end=' ')
+bst._print(bst.remove_elements_outside_range(bst.root, 100, 750))
+
+print('\nDelete a node in tree:', end=' ')
+bst._print(bst.delete(bst.root, 500))
+
+# print('\nConvert BST to circular doubly linked list:', end='\n ')
+# bst.bst_to_circular_doubly_linkedlist(bst.root, None)
+
+
