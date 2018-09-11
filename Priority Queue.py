dc40444b89b279a34f78d4cb4772b026523d1cc0
Shradha <shradha_kapoor@outlook.com>
2018-09-11 11:26:14 -0700

    Heaps

---
+++ Priority Queue.py
@@ -0,0 +1,16 @@
+
+
+# insert data with key
+
+# delete Min - delete element with smallest key
+
+# delete Max - delete element with largest key
+
+# get minimum - return element with smallest key without deleting
+
+# return kth smallest key
+
+# size of priority queue
+
+
+
--- binary_heaps.py
+++ binary_heaps.py
@@ -1,15 +1,70 @@
 
+# declare heap
 
+# build MinHeap
 
+# build MaxHeap
+
+# heapify an element - percolate up
+
+# heapify an element - percolate down
+
+# insert element in MinHeap
+
+# insert an element in MaxHeap
+
+# delete an arbitrary element from MinHeap
+
+# delete an ith indexed element from MinHeap
 
 # get maximum element in MinHeap
 
 # get minimum element in MinHeap
 
-# insert element
-
-# delete element
-
 # heap sort
 
-#
+# find all elements less than k in binary heap
+
+# merge 2 binary maxHeaps , size of first heap = m+n size of second heap = n , complexity O(m+n.log(m+n))
+
+# merge 2 binary maxHeaps , size of first heap = m+n size of second heap = n , complexity O(m+n)
+
+# get kth smallest element in MinHeap, complexity O(klogn)
+
+# get kth smallest element in MinHeap, complexity O(n)
+
+# get k max elements from maxHeap
+
+# implement stack using heap
+
+# implement queue using heap
+
+# given a big file with million of numbers, how do you find 10 maximum numbers from that file
+
+# merge k sorted lists with total n inputs altogether time complexity O(nk), space complexity O(1)
+
+# merge k sorted lists with total n inputs altogether time complexity O(nlogk), space complexity O(k) - using heaps
+
+# given 2 arrays A and B each with n elements, find largest n pairs (A[i],B[j])
+
+# min-max heap - structure that supports min and max in O(1) time, insert, deletemin, deletemax in O(logn) time
+
+# design heap data structure that supports finding the median in an infinite series of integers
+
+# maximum number in sliding window - brute force
+
+# maximum number in sliding winodw - using heap data structure
+
+# maximum number in sliding window - using double-ended-queue (perfect data structure for this problem)
+
+# merge(union) two binary heaps
+
+# find minimum window in inputString which will contain all chars in Pattern in complexity O(n).
+# if no such window, return empty string. If multiple such windows, return one unique minimum window
+
+# in maxHeap, check if kth largest item is >= x, runtime O(k)
+
+# Given k lists of sorted integers, find smallest range that includes atleast one number from each of k lists
+
+
+
