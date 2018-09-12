693232dd9c6bb5e4c451dd1f7e9b98591f77aa3d
Shradha <shradha_kapoor@outlook.com>
2018-09-12 10:56:34 -0700

    Searching

---
+++ Searching.py
@@ -0,0 +1,113 @@
+
+# unordered linear search
+
+# ordered/sorted linear search
+
+# binary search
+
+# check whether there are any duplicate elements in array of n numbers, time O(n^2) space O(1)
+
+# check whether there are any duplicate elements in array of n numbers, time O(n.logn) space O(1)
+
+# check whether there are any duplicate elements in array of n numbers, time O(n) space O(n)
+
+# check whether there are any duplicate elements in array of n numbers, time O(n) space O(1)
+
+# find element that appears maximum number of times in array, time O(n^2) space O(1)
+
+# find element that appears maximum number of times in array, time O(n.logn) space O(1)
+
+# find element that appears maximum number of times in array, time O(n) space O(n)
+
+# find first element which is repeated in an array, brute force
+
+# find first element which is repeated in an array, sorting technique
+
+# find first element which is repeated in an array, hashing technique
+
+# find the missing number - given a list of n-1 integers in range of 1 to n, no duplicates in the list.
+# simple solution ,time O(n^2) space O(1)
+
+# find the missing number - given a list of n-1 integers in range of 1 to n, no duplicates in the list.
+# sorting technique, time O(n.logn) space O(1)
+
+# find the missing number - given a list of n-1 integers in range of 1 to n, no duplicates in the list.
+# hashing technique, time O(n) space O(n)
+
+# find the missing number - given a list of n-1 integers in range of 1 to n, no duplicates in the list.
+# summation formula with XOR- get sum of numbers. time O(n) space O(1)
+
+# find the number occuring odd number of times - do a bitwise XOR
+
+# find two repeating elements in array, all elements are in range 1 to n. time O(n^2) space O(1)
+
+# find two repeating elements in array, all elements are in range 1 to n. time O(n.logn) space O(1) comparison sorting
+
+# find two repeating elements in array, all elements are in range 1 to n. time O(n) space O(n) hashing
+
+# find two repeating elements in array, all elements are in range 1 to n. time O(n) space O(1) XOR operation
+
+# find two repeating elements in array, all elements are in range 1 to n. time O(n) space O(1) sum and product formula
+
+# given numbers in range 1to n, n-1 elements repeat thrice, 1 element repeat twice, find the element.time O(n) space O(1)
+
+# given array A of n elements, find i j k such that A[i]^2 + A[j]^2 = A[k]^2
+
+# given array with both +ve and -ve nos. find 2 elements whose sum is closest to 0. brute force
+
+# given array with both +ve and -ve nos. find 2 elements whose sum is closest to 0. sorting
+
+# given array with both +ve and -ve nos. find 3 elements whose sum is closest to given element K. brute force
+
+# given array with both +ve and -ve nos. find 3 elements whose sum is closest to given element K. sorting
+
+# given array with both +ve and -ve nos. find 3 elements whose sum is closest to given element K. hashing and searching
+
+# given array with both +ve and -ve nos. find 3 elements whose sum is closest to 0. binary search
+
+# given array of unknown size with all 1s in beginning and 0s in end. find index in array from where 0 starts.
+
+# given sorted array of n integers that has been rotated unknown number of times, find an element in time O(logn)
+# example: find 5 in array (15 16 19 20 25 1 3 4 5 7 10 14), output index 8. solve using a variant of binary search
+
+# given sorted array of n integers that has been rotated unknown number of times, find an element in time O(logn)
+# example: find 5 in array (15 16 19 20 25 1 3 4 5 7 10 14), output index 8. Solve using recursion
+
+# given a bitonic array, find the index of maximum element in time O(logn)
+
+# given a sorted array A of n elements with duplicates, find index of first occurence of number p in time O(logn)
+
+# given a sorted array A of n elements with duplicates, find index of last occurence of number p in time O(logn)
+
+# given a sorted array A of n elements with duplicates, find number of occurences of number p in time O(n) linear search
+
+# given a sorted array A of n elements with duplicates, find number of occurences of number p . binary search
+
+# given an array of n elements, identify the element that appears more than n/2 times, time O(n^2) space O(1)
+
+# given an array of n elements, identify the element that appears more than n/2 times, time O(n.logn) space O(n)
+
+# given an array of n elements, identify the element that appears more than n/2 times, time O(n.logn) space O(1)
+
+# given an array of n elements, identify the element that appears more than n/2 times, time O(n) space O(1)
+
+# given array of 2n+1 elements, n elements appear twice and 1 integer appears once.find the integer in O(n) operations
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
