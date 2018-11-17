2f762abce4ea33d4b7030f25ab7204e16c483aff
Shradha <shradha_kapoor@outlook.com>
2018-11-16 17:58:28 -0800

    Divide and Conquer

---
+++ Divide and Conquer.py
@@ -0,0 +1,20 @@
+# Given an array with stock prices on n consecutive days. Find the day on which to buy stock and on which to sell stock
+# to make maximum profit. time O(n^2)
+
+# Given an array with stock prices on n consecutive days. Find the day on which to buy stock and on which to sell stock
+# to make maximum profit. time O(nlogn)
+
+# given a sequence of numbers a1, a2,...an. Find contiguous subsequence for which the sum of elements is maximum.
+# time O(nlogn) using divide and conquer
+
+# given a sequence of numbers a1, a2,...an. Find contiguous subsequence for which the sum of elements is maximum.
+# efficient solution using dynamic programming
+
+# Given set of n points S = p1,p2,...pn where pi= (xi,yi). Find pair of points having smallest distance among all pairs.
+# assume all points are in in 1D. time O(nlogn)
+
+# Given set of n points S = p1,p2,...pn where pi= (xi,yi). Find pair of points having smallest distance among all pairs.
+# assume all points are in in 2D. time O(n^2)
+
+# Given set of n points S = p1,p2,...pn where pi= (xi,yi). Find pair of points having smallest distance among all pairs.
+# assume all points are in in 2D. time O(nlogn) using divide and conquer
