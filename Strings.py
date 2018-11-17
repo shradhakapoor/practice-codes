49bcfd4ff5a3625d7f9c91aca6c0ac0ed2004e0c
Shradha <shradha_kapoor@outlook.com>
2018-11-16 17:13:27 -0800

    Strings

---
+++ Strings.py
@@ -0,0 +1,38 @@
+# Implement Trie to insert, search, delete a string
+
+# Implement Ternary Search Tree to insert, search, delete a string, Displaying all words of Tree
+
+# Implement Suffix Tree to insert, search, delete a string
+
+# Given a paragraph of words, find the word which appears maximum times. (use heap amd tries combination)
+
+# Given 2 strings, find the longest common substring, using suffix tree, time O(m+n)
+
+# Given text T, find the substring of T which is longest palindrome, using suffix tree
+
+# Reverse a string, time O(n), space O(n)
+
+# Reverse a string without using any temporary variable, time O(n), space O(1)
+
+# give an algorithm for matching pattern in text. Assume ? and * are wildcard chars.Brute Force. time O(mn) space O(1)
+
+# Reverse words in a sentence. time O(mn) space O(1)
+
+# Print all permutations of a string(anagrams)
+
+# Print all combinations of a string
+
+# Given a string 'ABCCBCBA', recursively remove adjacent characters if they are same.
+# for example, ABCCBCBA->ABBCBA->ACBA
+
+# Given a set of characters C and input string I, find minimum window in I that contains all chars in C.
+# example: I = ABBACBAA, C = AAB has minimum window BAA
+
+# Given 2 strings s1, s2. print all interleavings of s1, s2. Assume all chars in s1, s2 are different
+
+# Given a matrix nXn containing random numbers. Check whether the rows match with columns. use suffix tree.
+
+# Replace all spaces with '%20'. Assume string has sufficient space at end of string to hold additional characters.
+
+# Given a 2D board containing 'X' and 'O', capture all regions surrounded by 'X'. A region is captured by flipping all
+# 'O's into 'X's in that surrounded region.
