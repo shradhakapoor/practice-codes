a3ede8a0b62f45873eea7035d6138a574ae69351
Shradha <shradha_kapoor@outlook.com>
2018-11-29 15:42:26 -0800

    Dynamic Programming

---
+++ Dynamic Programming.py
@@ -0,0 +1,78 @@
+# given an array of n numbers, find a contiguous subsequence for which the sum of elements is maximum.
+# time O(n^3) space O(1)
+
+# given an array of n numbers, find a contiguous subsequence for which the sum of elements is maximum.
+# time O(n^2) space O(1)
+
+# given an array of n numbers, find a contiguous subsequence for which the sum of elements is maximum.
+# time O(n) space O(n)
+
+# given an array of n numbers, find a contiguous subsequence for which the sum of elements is maximum.
+# finding M(i) that indicates max sum over all windows ending at i.
+# time O(n) space O(1)
+
+# given an array of n numbers, find M(i) that indicates max sum over all windows starting at i and ending at n.
+# time O(n) space O(n)
+
+# given an array of n numbers, find M(i) that indicates max sum over all windows starting at 1 and ending at i.
+# condition: not to select two contiguous numbers. time O(n) space O(n)
+
+# given an array of n numbers, find M(i) that indicates max sum over all windows starting at i and ending at n.
+# condition: not to select two contiguous numbers. time O(n) space O(n)
+
+# given an array of n numbers, find M(i) that indicates max sum over all windows starting at 1 and ending at i.
+# condition: not to select three contiguous numbers. time O(n) space O(n)
+
+# given an array of n numbers, find M(i) that indicates max sum over all windows starting at i and ending at n.
+# condition: not to select three contiguous numbers. time O(n) space O(n)
+
+# given n petrol stations along a circular route. amount of petrol at station i is petrol[i]. you have car with
+# unlimited petrol tank and it costs cost[i] of petrol to travel from station i to its next station i+1. you begin the
+# journey with empty tank at one of the petrol stations, return the starting petrol station's index if you can travel
+# around circuit once , otherwise return -1.
+
+# given a series of matrices A1XA2XA3X...XAn with their dimensions, what is the best way to parenthesize them so that
+# it produces minimum number of total multiplications.
+
+# Integer knapsack problem(duplicate items allowed) given n types of items where si is integer size of ith item type,
+# vi is its value. We need to fill a knapsack of capacity C with items of maximum value.
+# Can add multiple items of same type to the knapsack. time O(nC) space O(C)
+
+# Integer knapsack problem(duplicate items allowed) given n types of items where si is integer size of ith item type,
+# vi is its value. We need to fill a knapsack of capacity C with items of maximum value.
+# each item is allowed to use for 0 or 1 time.
+
+# Making Change: given n types of coin denominations of values v1<v2<v3...<vn(integers). assume v1 = 1, so that we can
+# always make change for any amount of money C. find an algorithm which makes change for an amount of money C with as
+# few coins as possible.
+
+# given sequence of n numbers. determine a subsequence(not necessarily contiguous) of max length in which values in
+# subsequence form a strictly increasing sequence from position 1 to i.
+
+# given sequence of n numbers. determine a subsequence(not necessarily contiguous) of max length in which values in
+# subsequence form a strictly increasing sequence from position i to n.
+
+# Box Stacking: given a set of n rectangular 3D boxes. Dimensions of ith box are height hi, widhth wi, depth di.
+# Create a stack of boxes as tall as possible, but only stack a box on another box if dimensions of 2D base of lower box
+# are each strictly larger than those of 2D base of higher box. We can rotate a box so that any side functions as its
+# base.It is possible to use multiple instances of same type of box.
+
+# Determine whether a given set can be partitioned into two subsets such that the sum of elements in both subsets is
+# same. Recursive solution, time exponential
+
+# Determine whether a given set can be partitioned into two subsets such that the sum of elements in both subsets is
+# same. Dynamic programming solution, time O(sum X n)
+
+# given symbols 'true', 'false', 'and', 'or', 'xor'. find number of ways to parenthesize the expressions such that it
+# evaluates to true.
+
+# All pairs shortest path problem, Floyd's algorithm
+
+# consider a row of n coins of values v1,..., vn, where n is even(since its a 2 player game). We play this game with the
+# opponent.In each turn, a player selects either the first or last coin from the row, removes it from the row
+# permanently, and receives the value of coin. Determine maximum possible amount of money we can definitely win if we
+# move first.
+
+# given a sequence of length n, output length of longest palindrome subsequence. using DP time O(n^2)
+
+#
