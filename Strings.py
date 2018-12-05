8a6422a176c96ad466e07e90722442cc657cdbb5
Shradha <shradha_kapoor@outlook.com>
2018-12-04 21:49:11 -0800

    Strings

--- Strings.py
+++ Strings.py
@@ -1,4 +1,64 @@
+from collections import defaultdict
+
+
 # Implement Trie to insert, search, delete a string
+class TrieNode(object):
+    def __init__(self, label = None, data = None):
+        self.label = label
+        self.data = data
+
+        def defvalue():
+            return None
+        # key= character, value= list of other trie nodes or end_of_word node
+        self.children = defaultdict(defvalue)
+
+    def add_child(self, key, data=None):
+        if not isinstance(key, TrieNode):
+            self.children[key] = TrieNode(key, data)
+        else:
+            self.children[key.label] = key
+
+
+class Trie(object):
+    def __init__(self):
+        self.head = TrieNode()
+
+    def insert_string(self, inp):
+        word = inp
+        inp = list(inp.lower())
+        curr_node = self.head
+        end_of_inp = True
+        i = 0
+        # move till the inp already exists in Trie
+        while i < len(inp):
+            if inp[i] in curr_node.children:
+                curr_node = curr_node.children[inp[i]]
+                i += 1
+            else:
+                end_of_inp = False
+                break
+
+        # for every new character, create a new child node
+        if not end_of_inp:
+            while i < len(inp):
+                curr_node.add_child(inp[i])
+                curr_node = curr_node.children[inp[i]]
+                i += 1
+
+        # store full word at end node to avoid travel back to get whole word
+        curr_node.data = word
+
+
+    # def search_string(self, inp):
+    #     inp = list(inp.lower())
+    #     curr_node = self.head
+
+
+trie = Trie()
+words = 'hello goodbye help gerald gold tea ted team to too tom stan standard money'
+for word in words.split():
+    trie.insert_string(word)
+
 
 # Implement Ternary Search Tree to insert, search, delete a string, Displaying all words of Tree
 
