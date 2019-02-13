39531fcae079e7537a096792cf46fb5c0f5717ea
Shradha <shradha_kapoor@outlook.com>
2019-02-12 19:44:33 -0800

    Dynamic Programming

--- Dynamic Programming.py
+++ Dynamic Programming.py
@@ -1,58 +1,106 @@
 # given an array of n numbers, find a contiguous subsequence for which the sum of elements is maximum.
-# time O(n^3) space O(1)
-
-# given an array of n numbers, find a contiguous subsequence for which the sum of elements is maximum.
-# time O(n^2) space O(1)
-
-# given an array of n numbers, find a contiguous subsequence for which the sum of elements is maximum.
-# time O(n) space O(n)
-
-# given an array of n numbers, find a contiguous subsequence for which the sum of elements is maximum.
 # finding M(i) that indicates max sum over all windows ending at i.
 # time O(n) space O(1)
+def maximum_sum_subsequence(inp):
+    max_sum, current_sum = float('-inf'), 0
+    strt, ennd, s = 0, 0, 0
+    for i in range(0, len(inp)):
+        current_sum += inp[i]
+        if max_sum < current_sum:
+            max_sum = current_sum
+            strt = s
+            ennd = i
+        if current_sum < 0:
+            current_sum = 0
+            s = i+1
 
-# given an array of n numbers, find M(i) that indicates max sum over all windows starting at i and ending at n.
-# time O(n) space O(n)
+    return max_sum, strt, ennd
 
-# given an array of n numbers, find M(i) that indicates max sum over all windows starting at 1 and ending at i.
-# condition: not to select two contiguous numbers. time O(n) space O(n)
+res = maximum_sum_subsequence([-2, -3, 4, -1, -2, 1, 5, -3])
+print('contiguous subsequence where sum of elements is maximum: %d, start position: %d, end position: %d' %(res))
 
-# given an array of n numbers, find M(i) that indicates max sum over all windows starting at i and ending at n.
-# condition: not to select two contiguous numbers. time O(n) space O(n)
+# given an array of n positive numbers, find M(i) that indicates max sum over all windows starting at 1 and ending at i.
+# condition: not to select two contiguous numbers. time O(n)
+def max_sum_not_contiguous(inp):
+    # start with incl as first element in array, and excl as 0
+    incl, excl = inp[0], 0
 
-# given an array of n numbers, find M(i) that indicates max sum over all windows starting at 1 and ending at i.
-# condition: not to select three contiguous numbers. time O(n) space O(n)
+    for i in range(1, len(inp)):
+        temp = incl
+        # new incl is the max of (old incl and excl+current i)
+        incl = max(incl, excl+inp[i])
+        # assign old incl to excl
+        excl = temp
 
-# given an array of n numbers, find M(i) that indicates max sum over all windows starting at i and ending at n.
-# condition: not to select three contiguous numbers. time O(n) space O(n)
+    return incl
+
+print('Maximum sum for non adjacent elements:', max_sum_not_contiguous([5, 5, 10, 100, 10, 5]))
+
+# finding maximum sum non adjacent elements assuming its a circular array.
+# So first element cannot be with last element.
+def max_sum_non_adjacent_circular_array(inp):
+    if len(inp) == 0:
+        return 0
+    if len(inp) == 1:
+        return inp[0]
+
+    # find max sum from left to right ignoring the first element
+    incl = inp[1]
+    excl = 0
+    for i in range(2, len(inp)):
+        temp = incl
+        incl = max(incl, excl+inp[i])
+        excl = temp
+
+    # find max sum from right to left ignoring the last element
+    incl1 = inp[len(inp) -2]
+    excl1 = 0
+    for i in range(len(inp)-3, -1, -1):
+        temp1 = incl1
+        incl1 = max(incl1, excl1+inp[i])
+        excl1 = temp1
+
+    # Maximum of the two is the answer.It guarantees that both first and last element will not be selected together
+    return max(incl, incl1)
 
 # given n petrol stations along a circular route. amount of petrol at station i is petrol[i]. you have car with
 # unlimited petrol tank and it costs cost[i] of petrol to travel from station i to its next station i+1. you begin the
 # journey with empty tank at one of the petrol stations, return the starting petrol station's index if you can travel
-# around circuit once , otherwise return -1.
+# around circuit once , otherwise return -1. time O(n)
+class Petrol_Pump:
+    def __init__(self, petrol, distance):
+        self.petrol = petrol
+        self.distance = distance
 
-# given a series of matrices A1XA2XA3X...XAn with their dimensions, what is the best way to parenthesize them so that
-# it produces minimum number of total multiplications.
+def tour(inp):
+    n = len(inp)
+    # consider first petrol pump as starting point and form a queue data structure
+    start = 0
+    end = 1
+    current_petrol = inp[start].petrol - inp[start].distance
 
-# Integer knapsack problem(duplicate items allowed) given n types of items where si is integer size of ith item type,
-# vi is its value. We need to fill a knapsack of capacity C with items of maximum value.
-# Can add multiple items of same type to the knapsack. time O(nC) space O(C)
+    while start!=end or current_petrol<0:
 
-# Integer knapsack problem(duplicate items allowed) given n types of items where si is integer size of ith item type,
-# vi is its value. We need to fill a knapsack of capacity C with items of maximum value.
-# each item is allowed to use for 0 or 1 time.
+        # while we have not reached the end but the current_petrol has become negative
+        while start!=end and current_petrol<0:
+            # remove the starting petrol pump and change start
+            current_petrol = current_petrol - inp[start].petrol + inp[start].distance
+            start = (start+1)%n
 
-# Making Change: given n types of coin denominations of values v1<v2<v3...<vn(integers). assume v1 = 1, so that we can
-# always make change for any amount of money C. find an algorithm which makes change for an amount of money C with as
-# few coins as possible.
+            # if we reached petrol pump 0 again, then there is no possible solution
+            if start == 0:
+                return -1
 
-# given sequence of n numbers. determine a subsequence(not necessarily contiguous) of max length in which values in
-# subsequence form a strictly increasing sequence from position 1 to i.
+        # when we have current_petrol as positive and want to move to next petrol pump
+        current_petrol = current_petrol + inp[end].petrol - inp[end].distance
+        end = (end+1) %n
 
-# given sequence of n numbers. determine a subsequence(not necessarily contiguous) of max length in which values in
-# subsequence form a strictly increasing sequence from position i to n.
+    return start
 
-# Box Stacking: given a set of n rectangular 3D boxes. Dimensions of ith box are height hi, widhth wi, depth di.
+arr = [Petrol_Pump(6,4), Petrol_Pump(3,6), Petrol_Pump(7,3)]
+print('starting petrol pump possible:', tour(arr))
+
+# Box Stacking: given a set of n rectangular 3D boxes. Dimensions of ith box are height hi, width wi, depth di.
 # Create a stack of boxes as tall as possible, but only stack a box on another box if dimensions of 2D base of lower box
 # are each strictly larger than those of 2D base of higher box. We can rotate a box so that any side functions as its
 # base.It is possible to use multiple instances of same type of box.
@@ -86,10 +134,29 @@
 
 # given nXn matrix of +ve and -ve integers, find the sub-matrix with largest possible sum.
 
-# kadane's algorithm. time O(n^3)
-
 # find optimal number of jumps to start from first element to last element, jump length at a time can be atmost the
 # value at current position in array. optimum result is reaching in minimum number of jumps.
 
+# given an array of n numbers, find M(i) that indicates max sum over all windows starting at 1 and ending at i.
+# condition: not to select three contiguous numbers.
 
+# given a series of matrices A1XA2XA3X...XAn with their dimensions, what is the best way to parenthesize them so that
+# it produces minimum number of total multiplications.
 
+# Integer knapsack problem(duplicate items allowed) given n types of items where si is integer size of ith item type,
+# vi is its value. We need to fill a knapsack of capacity C with items of maximum value.
+# Can add multiple items of same type to the knapsack. time O(nC) space O(C)
+
+# Integer knapsack problem(duplicate items allowed) given n types of items where si is integer size of ith item type,
+# vi is its value. We need to fill a knapsack of capacity C with items of maximum value.
+# each item is allowed to use for 0 or 1 time.
+
+# Making Change: given n types of coin denominations of values v1<v2<v3...<vn(integers). assume v1 = 1, so that we can
+# always make change for any amount of money C. find an algorithm which makes change for an amount of money C with as
+# few coins as possible.
+
+# given sequence of n numbers. determine a subsequence(not necessarily contiguous) of max length in which values in
+# subsequence form a strictly increasing sequence from position 1 to i.
+
+# given sequence of n numbers. determine a subsequence(not necessarily contiguous) of max length in which values in
+# subsequence form a strictly increasing sequence from position i to n.
